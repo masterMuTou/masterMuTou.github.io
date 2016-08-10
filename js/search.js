82810e75854dbf74078d100ba1f439d48e1f7922
木头 <micmrshi@outlook.com>
2016-08-10 09:43:51 +0800

    Site updated: 2016-08-10 09:43:50

--- js/search.js
+++ js/search.js
@@ -23,21 +23,21 @@ var searchFunc = function(path, search_id, content_id) {
     $.ajax({
         url: path,
         dataType: "xml",
-        success: function( xmlResponse ) {
+        success: function(xmlResponse) {
             // get the contents from search data
-            var datas = $( "entry", xmlResponse ).map(function() {
+            var datas = $("entry", xmlResponse).map(function() {
                 return {
-                    title: $( "title", this ).text(),
-                    content: $("content",this).text(),
-                    url: $( "url" , this).text()
+                    title: $("title", this).text(),
+                    content: $("content", this).text(),
+                    url: $("url", this).text()
                 };
             }).get();
 
             var $input = document.getElementById(search_id);
             var $resultContent = document.getElementById(content_id);
 
-            $input.addEventListener('input', function(){
-                var str='<ul class=\"search-result-list\">';                
+            $input.addEventListener('input', function() {
+                var str = '<ul class=\"search-result-list\">';
                 var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
                 $resultContent.innerHTML = "";
                 if (this.value.trim().length <= 0) {
@@ -46,12 +46,12 @@ var searchFunc = function(path, search_id, content_id) {
                 // perform local searching
                 datas.forEach(function(data) {
                     var isMatch = true;
-                    var content_index = [];                                                       
+                    var content_index = [];
                     if (!data.title || data.title.trim() === '') {
                         data.title = "Untitled";
                     }
-                    var data_title = data.title.trim().toLowerCase();     
-                    var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
+                    var data_title = data.title.trim().toLowerCase();
+                    var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
                     var data_url = data.url;
                     var index_title = -1;
                     var index_content = -1;
@@ -62,7 +62,7 @@ var searchFunc = function(path, search_id, content_id) {
                             index_title = data_title.indexOf(keyword);
                             index_content = data_content.indexOf(keyword);
 
-                            if( index_title < 0 && index_content < 0 ){
+                            if (index_title < 0 && index_content < 0) {
                                 isMatch = false;
                             } else {
                                 if (index_content < 0) {
@@ -79,34 +79,34 @@ var searchFunc = function(path, search_id, content_id) {
                     }
                     // show search results
                     if (isMatch) {
-                        str += "<li><a href='"+ data_url +"' class='search-result-title'>"+ data_title +"</a>";
-                        var content = data.content.trim().replace(/<[^>]+>/g,"");
+                        str += "<li><a href='" + data_url + "' class='search-result-title'>" + data_title + "</a>";
+                        var content = data.content.trim().replace(/<[^>]+>/g, "");
                         if (first_occur >= 0) {
                             // cut out 100 characters
                             var start = first_occur - 20;
                             var end = first_occur + 80;
 
-                            if(start < 0){
+                            if (start < 0) {
                                 start = 0;
                             }
 
-                            if(start == 0){
+                            if (start == 0) {
                                 end = 100;
                             }
 
-                            if(end > content.length){
+                            if (end > content.length) {
                                 end = content.length;
                             }
 
-                            var match_content = content.substr(start, end); 
+                            var match_content = content.substr(start, end);
 
                             // highlight all keywords
-                            keywords.forEach(function(keyword){
+                            keywords.forEach(function(keyword) {
                                 var regS = new RegExp(keyword, "gi");
-                                match_content = match_content.replace(regS, "<em class=\"search-keyword\">"+keyword+"</em>");
+                                match_content = match_content.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
                             });
-                            
-                            str += "<p class=\"search-result\">" + match_content +"...</p>"
+
+                            str += "<p class=\"search-result\">" + match_content + "...</p>"
                         }
                         str += "</li>";
                     }
